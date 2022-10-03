d10c0367a0f306ddad1aa1f36f88442df72c4da6
Ersin <ersin.nurtin@outlook.com>
2022-10-04 00:23:07 +0300

    Updated hangman.py
    fixed some bugs and added continuous playing

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,5 +1,7 @@
 import random
 
+mistake_counter = 0
+
 
 def hangman_pics(counter):
     pics = ["  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
@@ -15,57 +17,61 @@ def hangman_pics(counter):
             "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
 
             "  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="]
-    return print(pics[counter[0]])
+    return print(pics[counter])
 
 
-def letter_not_in_word(guessed, count, output, original):
-    count[0] += 1
-    if count[0] == 6:
-        hangman_pics(count)
-        print("Game is over! You lost!")
-        print(f"Original word: {original}")
-        exit()
-    print(output)
-    hangman_pics(count)
-    print(f"'{guessed}' is not in the word! You have "
-          f"{6 - count[0]} attempts left before getting "
+def ask_for_input():
+    user_input = input("Guess a letter or word: ").upper()
+    return user_input
+
+
+def letter_not_in_word(guessed_letter, counter, original_word):
+    counter += 1
+    if counter == 6:
+        hangman_pics(counter)
+        print("Game over! You lost!")
+        print(f"Original word: {original_word}")
+        game_on()
+    print(f"'{guessed_letter}' is not in the word! You have "
+          f"{6 - counter} attempts left before getting "
           f"hanged!")
+    return counter
 
 
-def check_guessed_letter(letter, list_of_letters, word, guess_word,
-                         count_of_mistakes, output, original):
+def check_guessed_letter(letter, list_of_letters, word, guess_word):
     if letter.isalpha():
-        if letter not in list_of_letters:
+        if letter in list_of_letters:
+            print("You already guessed that letter!")
+            check_guessed_letter(ask_for_input(), list_of_letters, word, guess_word)
+        else:
             list_of_letters.append(letter)
             if letter in word and letter not in guess_word:
                 return True
-            else:
-                letter_not_in_word(letter, count_of_mistakes, output, original)
-        else:
-            return False
+    return False
 
 
-def main_game(word, original):
+def main_game(word, original, counter):
     guess_word = list(len(word) * "_")
     msg = f"Current word length: {len(word)}"
     output = "".join(guess_word)
     guessed_letters = []
-    mistake_counter = [0]
     while not output.isalpha():
-        print(msg, "\n", output)
-        hangman_pics(mistake_counter)
-        guess_letter = input("Guess a letter or word: ").upper()
+        print(msg, output)
+        hangman_pics(counter)
+        guess_letter = ask_for_input()
 
-        while not check_guessed_letter(guess_letter, guessed_letters,
-                         word, guess_word, mistake_counter, output, original):
-            guess_letter = input("Guess a letter or word: ").upper()
+        if not check_guessed_letter(guess_letter, guessed_letters,
+                                    word, guess_word):
+            counter = letter_not_in_word(guess_letter, counter, original)
+            continue
 
         for index in range(len(word)):
             if word[index] == guess_letter:
                 guess_word[index] = guess_letter
         word = word.replace(guess_letter, "*")
         output = "".join(guess_word)
-    return print("Congratulations! You win!")
+    print("Congratulations! You win!")
+    game_on()
 
 
 def check_game(word):
@@ -91,7 +97,7 @@ def game_on():
                          "computer:\n\n-").upper()
     initial_word = check_game(initial_word)
     original_word = initial_word
-    main_game(initial_word, original_word)
+    main_game(initial_word, original_word, mistake_counter)
 
 
 game_on()
